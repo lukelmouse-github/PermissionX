955027f8362e7ed04955bfcb68fe18e63bd211ca
jiangjia <jiangjia.jia@bytedance.com>
2022-07-14 18:10:04 +0800

    完成PermissionX库的开发与测试工作

--- app/src/main/java/com/permissionx/app/MainActivity.kt
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.permissionx.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.permissionx.lukelmouse.PermissionX
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val makeCallBtn = findViewById<Button>(R.id.makeCallBtn)
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 }
